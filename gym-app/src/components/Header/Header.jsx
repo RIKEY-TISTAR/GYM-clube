f8277fcaeb1a783be4e4ae6f2c159274dc3f946b
RIKEY-TISTAR <agabeckov.ruslan13@yandex.ru>
2024-04-10 21:14:39 +0300

    -_-

--- gym-app/src/components/Header/Header.jsx
+++ gym-app/src/components/Header/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Header = () => {
   return (
     <header className='header'>
-      <div className="container mx-auto w-full">
+      <div className="container mx-auto w-full absolute z-10">
 
-        <nav className="bg-gray-800">
+        <nav className="bg-transparent">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
 
@@ -40,16 +40,16 @@ export const Header = () => {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                    <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Dashboard</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Team</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Calendar</a>
+                    <a href="#" className=" text-gray-300 hover:text-white rounded-md px-3 py-2 text-mb font-medium" aria-current="page">Dashboard</a>
+                    <a href="#" className="text-gray-300 hover:text-white rounded-md px-3 py-2 text-mb font-medium">Team</a>
+                    <a href="#" className="text-gray-300 hover:text-white rounded-md px-3 py-2 text-mb font-medium">Projects</a>
+                    <a href="#" className="text-gray-300 hover:text-white rounded-md px-3 py-2 text-mb font-medium">Calendar</a>
                   </div>
                 </div>
               </div>
 
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <button type="button" className="relative rounded-full bg-transparent p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">View notifications</span>
                   <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
@@ -76,12 +76,13 @@ export const Header = () => {
                   From: "transform opacity-100 scale-100"
                   To: "transform opacity-0 scale-95"
           --> */}
-                  <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                    {/* <!-- Active: "bg-gray-100", Not Active: "" --> */}
+                  {/* <div className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                    <!-- Active: "bg-gray-100", Not Active: "" -->
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
                     <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-                  </div>
+                  </div> */}
+                  
                 </div>
               </div>
 
--- gym-app/src/components/Hero/Hero.jsx
+++ gym-app/src/components/Hero/Hero.jsx
@@ -4,17 +4,38 @@ import Heroimg from '/public/assets/images/hero/bg.jpg';
 
 export const Hero = () => {
   return (
-    <section className='hero '>
-      <div className="container mx-auto w-full">
-        
-        <div className="bg relative w-full h-auto">
-          {/* <img className='w-full h-full object-cover absolute mix-blend-owerlay' src="/public/assets/images/Hero/BG.jpg" alt="" /> */}
-           <Image width={1920} height={1080} className='object-cover absolute mix-blend-owerlay' src={Heroimg} alt="-_-" />
+    <section className='hero'>
+
+      <div className="bg relative w-full">
+
+        <div className="object-cover absolute mix-blend-owerlay -z-10">
+          <Image width={1920} height={1080} className='w-full' src={Heroimg} alt="-_-" />
+        </div>
+        {/* <img className='w-full h-full object-cover absolute mix-blend-owerlay' src="/public/assets/images/Hero/BG.jpg" alt="" /> */}
+
+        <div className="text-section container mx-auto w-full ">
+
+          <div className="hero-title-section full mb-4">
+            <h2 className=" hero-title text-center text-white">Достигните своих целей в области здоровья и фитнеса в кратчайшие сроки</h2>
+          </div>
+
+          <div className="hero-subtitle-section w-full mb-4">
+            <p className="hero-subtitle text-center text-white">
+            Неважно, хотите ли вы стать сильнее,
+            сжечь жир или просто оставаться в форме,
+            наши тренеры мирового класса будут сопровождать вас на каждом этапе пути.
+            </p>
+          </div>
+
+          <div className="button-section">
+            <button className='hero-btn bg-yellow-300 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded-full'>Начать 7-дневную бесплатную пробную версию</button>
+          </div>
+
         </div>
 
       </div>
+
+
     </section>
   )
 }
-      {/* <div style="background-image: url('/assets/img/Hero/BG.jpg');"  className="img"></div> */}
-// bg-[url("https://images.unsplash.com/photo-1593079831268-3381b0db4a77?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")]
