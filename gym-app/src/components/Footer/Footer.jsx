d7c158d6a6a38e830c41935f566feebdd9a46100
RIKEY-TISTAR <agabeckov.ruslan13@yandex.ru>
2024-04-12 19:43:35 +0300

    ХЕДЕР НЕ ЗДЕЛАН

--- gym-app/src/components/Footer/Footer.jsx
+++ gym-app/src/components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Image from 'next/image';
+import TimeTablework from '/public/assets/images/time-table/dumbbell.png';
 
 export const Footer = () => {
   return (
 
-    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 w-full">
+    <footer className=" rounded-lg shadow bg-neutral-900 w-full">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <Image width={1920} height={1080} className='stock-icon-img w-full' src={TimeTablework} alt="-_-" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
